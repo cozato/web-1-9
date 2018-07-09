0a4d5f5a33b48abd4270c2eb0eb1d800c07a53ba
tkskd <g1727038@st.t-kougei.ac.jp>
2018-07-09 18:24:50 +0900

    AppleFall

---
+++ AppleFall/Assets/chapter8/ItemController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemController : MonoBehaviour {
+
+    // Use this for initialization
+    public float dropSpeed = -0.03f;
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(0, this.dropSpeed, 0);
+        if(transform.position.y < -1.0f)
+        {
+            Destroy(gameObject);
+        }
+	}
+}
